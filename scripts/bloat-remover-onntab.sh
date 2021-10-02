2b0f6e975efb1927a98361eaac3330082bafcc95
Ian Kluft <github-dev@iankluft.com>
2021-10-02 01:59:10 -0700

    add text to the README.md description of bloat-remover-onntab.sh from a comment I made pointing here on XDA Developers. And sync the description text into the script header comments.

--- scripts/bloat-remover-onntab.sh
+++ scripts/bloat-remover-onntab.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 # bloatware app remover for Onn Android tablets (based on apps I found on a 2020-model 10.1" tablet)
 #
+# bloat-remover-onntab.sh is a script to remove bloatware apps from an Onn (Walmart) Android tablet. This worked for
+# a 2020-model 10.1 inch tablet. Review and modify the script as needed for your own use. This is for use on Linux
+# or other Unix-like systems. You must have ADB, and may (or may not) need root access on your desktop/laptop system
+# to run ADB. It requires developer access on the tablet, but not root. Of course, since it uninstalls apps, I have
+# to re-emphasize what is already the case from the GPLv3 license that you use it at your own risk. I posted it
+# because I see people asking online. Warning: you can easily disable your device by removing system apps. Don't
+# remove anything you don't understand. You have been warned.
+#
 # Copyright 2021 by Ian Kluft
 # released as Open Source Software under the GNU General Public License Version 3.
 # See https://www.gnu.org/licenses/gpl.txt
