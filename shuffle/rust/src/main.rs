f7ff3234158615a4a9708906553762c689194832
Ian Kluft <github-dev@iankluft.com>
2022-08-24 16:52:27 -0700

    get rid of panics: main displays results or errors

--- shuffle/rust/src/main.rs
+++ shuffle/rust/src/main.rs
@@ -13,42 +13,44 @@
 
 use rand::{seq::SliceRandom, thread_rng};
 use std::{
-    env,
+    env::args,
     fs::File,
     io,
     io::{BufRead, BufReader},
     iter::Iterator,
     path::Path,
+    result::Result,
+    process::ExitCode,
 };
 
 // read a file into a vector of strings
-fn read_file_lines(infile_path: &Path) -> Vec<String> {
-    let infile = match File::open(&infile_path) {
-        Err(why) => panic!(
-            "could not open input file {}: {}",
-            infile_path.display(),
-            why
-        ),
-        Ok(f) => f,
-    };
+fn read_file_lines(infile_path: &Path) -> Result<Vec<String>, io::Error> {
+    let infile = File::open(&infile_path)?;
     let reader = BufReader::new(infile);
-    reader
+    Ok(reader
         .lines()
         .filter_map(io::Result::ok)
-        .collect::<Vec<String>>()
+        .collect::<Vec<String>>())
 }
 
 // mainline - read file, shuffle it, output it
-fn main() -> io::Result<()> {
+fn main() -> ExitCode {
     // get input file name from command line
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = args().collect();
     if args.len() < 2 {
-        panic!("file name parameter missing");
+        eprintln!("file name parameter missing");
+        return ExitCode::FAILURE;
     }
     let infile_path = Path::new(&args[1]);
 
     // read input file to vector
-    let mut lines = read_file_lines(&infile_path);
+    let mut lines = match read_file_lines(&infile_path) {
+        Err(why) => {
+            eprintln!("file read failed: {}", why);
+            return ExitCode::FAILURE;
+        },
+        Ok(vecstr) => vecstr,
+    };
 
     // shuffle vector
     let mut rng = thread_rng();
@@ -60,5 +62,5 @@ fn main() -> io::Result<()> {
     }
 
     // done
-    Ok(())
+    ExitCode::SUCCESS
 }
