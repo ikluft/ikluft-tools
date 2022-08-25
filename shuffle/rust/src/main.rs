52483ce59886b416973cc49dc17108d6e7eed70d
Ian Kluft <github-dev@iankluft.com>
2022-08-25 15:09:07 -0700

    clean up error messages, add file checks

--- shuffle/rust/src/main.rs
+++ shuffle/rust/src/main.rs
@@ -10,7 +10,7 @@
 //
 // usage: shuffle input.txt > output.txt
 
-use anyhow::{anyhow, Context, Result};
+use anyhow::{bail, Context, Result};
 use rand::{seq::SliceRandom, thread_rng};
 use std::{
     env::args,
@@ -21,11 +21,14 @@ use std::{
     path::Path,
 };
 
+// constants
+const MAX_FILE_SIZE: u64 = 1 << 16; // 64K max - arbitrary but in-memory algorithm is only for small files
+
 // read a file into a vector of strings
 fn read_file_lines(infile_path: &Path) -> Result<Vec<String>, anyhow::Error> {
     let infile = File::open(&infile_path).with_context(|| {
         format!(
-            "Failed to read instrs from {}",
+            "Failed to open {}",
             infile_path.to_string_lossy()
         )
     })?;
@@ -41,9 +44,21 @@ fn main() -> Result<()> {
     // get input file name from command line
     let args: Vec<String> = args().collect();
     if args.len() < 2 {
-        return Err(anyhow!("file name parameter missing"));
+        bail!("file name parameter missing");
     }
     let infile_path = Path::new(&args[1]);
+
+    // basic file checks
+    if ! infile_path.exists() {
+        bail!("path does not exist: {}", infile_path.to_string_lossy());
+    }
+    if ! infile_path.is_file() {
+        bail!("path is not a regular file: {}", infile_path.to_string_lossy());
+    }
+    let infile_metadata = infile_path.metadata()?;
+    if infile_metadata.len() > MAX_FILE_SIZE {
+        bail!("file is too large for in-memory shuffle algorithm: {}", infile_path.to_string_lossy());
+    }
 
     // read input file to vector
     let mut lines = read_file_lines(&infile_path)?;
