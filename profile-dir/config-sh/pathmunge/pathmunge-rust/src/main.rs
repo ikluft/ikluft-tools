04600dbed86ce0106f7323a0c6834999f36c6e34
Ian Kluft <github-dev@iankluft.com>
2022-10-10 15:28:16 -0700

    make delimiter a parameter; some code reorg

--- profile-dir/config-sh/pathmunge/pathmunge-rust/src/main.rs
+++ profile-dir/config-sh/pathmunge/pathmunge-rust/src/main.rs
@@ -14,7 +14,11 @@ use std::{
 
 // constants
 const DEFAULT_VAR_NAME: &str = "PATH";
-const DEFAULT_DELIMETER: &str = ":";
+const DEFAULT_DELIMITER: &str = ":";
+const BEFORE_PARAM : &str = "before";
+const AFTER_PARAM : &str = "after";
+const VAR_PARAM : &str = "var";
+const DELIMITER_PARAM : &str = "delimiter";
 
 // add to Unix PATH or similar environment variable with deduplication
 fn main() -> Result<()> {
@@ -22,43 +26,54 @@ fn main() -> Result<()> {
     let cli = Command::new("pathmunge")
         .about("add to a Unix PATH or similar environment variable with deduplication")
         .arg(
-            Arg::new("before")
-                .long("before")
+            Arg::new(BEFORE_PARAM)
+                .long(BEFORE_PARAM)
                 .value_name("PATH")
                 .num_args(1),
         )
         .arg(
-            Arg::new("after")
-                .long("after")
+            Arg::new(AFTER_PARAM)
+                .long(AFTER_PARAM)
                 .value_name("PATH")
                 .num_args(1),
         )
         .group(
             ArgGroup::new("position")
-                .args(["before", "after"])
+                .args([BEFORE_PARAM, AFTER_PARAM])
                 .multiple(true)
                 .required(true),
         )
         .arg(
-            Arg::new("var")
-                .long("var")
+            Arg::new(VAR_PARAM)
+                .long(VAR_PARAM)
                 .num_args(1)
                 .required(false)
                 .value_name("VARNAME")
                 .default_value(DEFAULT_VAR_NAME),
+        )
+        .arg(
+            Arg::new(DELIMITER_PARAM)
+                .long(DELIMITER_PARAM)
+                .num_args(1)
+                .required(false)
+                .value_name("DELIMITER")
+                .default_value(DEFAULT_DELIMITER),
         );
 
     // check for errors
     let result = cli.try_get_matches();
     let matches = result?; // unwrap matches from result or end with CLI error
 
-    // extract values
-    let before = matches.get_one::<String>("before");
-    let after = matches.get_one::<String>("after");
-    let var_name = matches.get_one::<String>("var");
+    // extract values as Options since these may be missing
+    let before : Option<&String> = matches.get_one::<String>(BEFORE_PARAM);
+    let after : Option<&String> = matches.get_one::<String>(AFTER_PARAM);
+
+    // extract and unwrap values which are protected by a default value
+    let var_name : &String = matches.get_one::<String>(VAR_PARAM).unwrap();
+    let delimiter : &str = matches.get_one::<String>(DELIMITER_PARAM).unwrap().as_str();
 
     // read the specified environment variable (default PATH), if it exists
-    let env_value = env::var(var_name.unwrap_or(&DEFAULT_VAR_NAME.to_string()));
+    let env_value = env::var(var_name);
 
     // assemble path element strings
     let mut elements: Vec<String> = Vec::new();
@@ -78,7 +93,7 @@ fn main() -> Result<()> {
     let mut dirs_seen: HashSet<String> = HashSet::new();
     for element in elements.iter() {
         // split element into directory strings
-        let dir_strs: Vec<&str> = element.split(DEFAULT_DELIMETER).collect();
+        let dir_strs: Vec<&str> = element.split(delimiter).collect();
         for dir_str in dir_strs.into_iter() {
             // skip entries already in dirs_seen set
             if dirs_seen.contains(dir_str) {
@@ -103,7 +118,7 @@ fn main() -> Result<()> {
     }
 
     // join path with separator and print it
-    let path_str: String = path_out.join(DEFAULT_DELIMETER);
+    let path_str: String = path_out.join(delimiter);
     println!("{}", path_str);
 
     // done
