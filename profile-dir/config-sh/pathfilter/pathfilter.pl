50f0b93464add05c76b933ab68728e8364d4cf52
Ian Kluft <github-dev@iankluft.com>
2022-10-11 14:18:33 -0700

    upgrade pathfilter.pl to match features of Rust implementation: canonicalize paths, set delimiter

--- profile-dir/config-sh/pathfilter/pathfilter.pl
+++ profile-dir/config-sh/pathfilter/pathfilter.pl
@@ -13,27 +13,38 @@ use warnings;
 use 5.24.0;
 use utf8;
 use Getopt::Long;
+use Cwd qw(abs_path);
 
 # globals
 my ($debug, @before, @after, @path, %path);
 my $var = "PATH";
+my $delimiter = ":";
 
 # fetch before/after path elements from command line
-GetOptions("debug" => \$debug, "before:s" => \@before, "after:s" => \@after, "var:s" => \$var);
+GetOptions("debug" => \$debug, "before:s" => \@before, "after:s" => \@after, "var:s" => \$var,
+    "delimiter:s" => \$delimiter);
 
 # load before/path/after elements and deduplicate
-foreach my $dir (map {split /:/, $_} @before, ($ENV{$var} // ()), @after) {
+foreach my $dir (map {split /$delimiter/x, $_} @before, ($ENV{$var} // ()), @after) {
     $debug and say STDERR "debug: found $dir";
 	if ($dir eq "." ) {
         # omit "." for good security practice
         next;
     }
 
+    # skip if the path doesn't exist or isn't a directory
+    if (not -e $dir or not -d $dir) {
+        next
+    }
+
+    # convert to canonical path
+    my $abs_dir = abs_path($dir);
+
     # add the path if it hasn't already been seen, and it exists
-	if (not exists $path{$dir} and -d $dir) {
-        push @path, $dir;
-        $debug and say STDERR "debug: pushed $dir";
+	if (not exists $path{$abs_dir} and -d $abs_dir) {
+        push @path, $abs_dir;
+        $debug and say STDERR "debug: pushed $abs_dir";
 	}
-	$path{$dir} = 1;
+	$path{$abs_dir} = 1;
 }
-say join ":", @path;
+say join $delimiter, @path;
