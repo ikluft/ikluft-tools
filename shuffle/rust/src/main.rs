681577b83e07cc4f6c01836d02d10f49e1ccad2c
Ian Kluft <ian.kluft+github@gmail.com>
2022-10-17 16:53:04 -0700

    reorg some mainline code into function

--- shuffle/rust/src/main.rs
+++ shuffle/rust/src/main.rs
@@ -18,15 +18,40 @@ use std::{
     io,
     io::{BufRead, BufReader},
     iter::Iterator,
-    path::Path,
+    path::{Path, PathBuf},
 };
 
 // constants
 const MAX_FILE_SIZE: u64 = 1 << 16; // 64K max - arbitrary but in-memory algorithm is only for small files
 
+// get file path from command line
+fn path_from_cli() -> Result<PathBuf, anyhow::Error> {
+    // get input file name from command line
+    let args: Vec<String> = args().collect();
+    if args.len() < 2 {
+        bail!("file name parameter missing");
+    }
+    let infile_path = Path::new(&args[1]).to_owned();
+
+    // basic file checks
+    if ! infile_path.exists() {
+        bail!("path does not exist: {}", infile_path.to_string_lossy());
+    }
+    if ! infile_path.is_file() {
+        bail!("path is not a regular file: {}", infile_path.to_string_lossy());
+    }
+    let infile_metadata = infile_path.metadata()?;
+    if infile_metadata.len() > MAX_FILE_SIZE {
+        bail!("file is too large for in-memory shuffle algorithm: {}", infile_path.to_string_lossy());
+    }
+
+    // return path
+    Ok(infile_path)
+}
+
 // read a file into a vector of strings
-fn read_file_lines(infile_path: &Path) -> Result<Vec<String>, anyhow::Error> {
-    let infile = File::open(&infile_path).with_context(|| {
+fn read_file_lines(infile_path: &PathBuf) -> Result<Vec<String>, anyhow::Error> {
+    let infile = File::open(infile_path).with_context(|| {
         format!(
             "Failed to open {}",
             infile_path.to_string_lossy()
@@ -41,24 +66,8 @@ fn read_file_lines(infile_path: &Path) -> Result<Vec<String>, anyhow::Error> {
 
 // mainline - read file, shuffle it, output it
 fn main() -> Result<()> {
-    // get input file name from command line
-    let args: Vec<String> = args().collect();
-    if args.len() < 2 {
-        bail!("file name parameter missing");
-    }
-    let infile_path = Path::new(&args[1]);
-
-    // basic file checks
-    if ! infile_path.exists() {
-        bail!("path does not exist: {}", infile_path.to_string_lossy());
-    }
-    if ! infile_path.is_file() {
-        bail!("path is not a regular file: {}", infile_path.to_string_lossy());
-    }
-    let infile_metadata = infile_path.metadata()?;
-    if infile_metadata.len() > MAX_FILE_SIZE {
-        bail!("file is too large for in-memory shuffle algorithm: {}", infile_path.to_string_lossy());
-    }
+    // get file path from command line
+    let infile_path = path_from_cli()?;
 
     // read input file to vector
     let mut lines = read_file_lines(&infile_path)?;
