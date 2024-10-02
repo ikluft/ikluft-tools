a01c909a6eef618683c6fbb1e11d9a25b50050a6
Ian Kluft <ko6yq.dev+github@gmail.com>
2024-10-01 23:51:06 -0700

    new alerts core module will be called AlertGizmo

---
+++ space-alerts/lib/AlertGizmo.pm
@@ -0,0 +1,36 @@
+# AlertGizmo
+# ABSTRACT: base class for AlertGizmo feed monitors
+# Copyright (c) 2024 by Ian Kluft
+
+# pragmas to silence some warnings from Perl::Critic
+## no critic (Modules::RequireExplicitPackage)
+# This solves a catch-22 where parts of Perl::Critic want both package and use-strict to be first
+use Modern::Perl qw(2023);   # includes strict & warnings, boolean requires 5.36, try/catch requires 5.34
+## use critic (Modules::RequireExplicitPackage)
+
+package AlertGizmo;
+
+use utf8;
+use autodie;
+use experimental qw(builtin try);
+use feature      qw(say try);
+use builtin      qw(true false);
+
+#
+# common functions used by AlertGizmo feed monitors
+#
+
+# convert DateTime to date/time/tz string
+sub dt2dttz
+{
+    my $dt = shift;
+    return $dt->ymd('-') . " " . $dt->hms(':') . " " . $dt->time_zone_short_name();
+}
+
+# 
+sub main
+{
+    # TODO
+}
+
+1;
