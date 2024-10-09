80d6252b2b504da9a3f13f008b4f0bdb77e0adde
Ian Kluft <ko6yq.dev+github@gmail.com>
2024-10-08 23:27:51 -0700

    WIP: continue writing Config class - paths, result type processing

--- space-alerts/lib/AlertGizmo/Config.pm
+++ space-alerts/lib/AlertGizmo/Config.pm
@@ -12,10 +12,16 @@ package AlertGizmo::Config;
 
 use utf8;
 use autodie;
-use parent qw(Class::Singleton);
-use Carp qw(croak confess);
+use parent       qw(Class::Singleton);
+use experimental qw(builtin try);
+use feature      qw(say try);
+use builtin      qw(true false);
+use Carp         qw(confess);
+use results;
+use results::exceptions qw( NotFound NonIntegerIndex InvalidNodeType NoAutoArray );
 
 # helper function to allow methods to get the singleton instance whether called as a class or instance method
+# private class function
 sub _class_or_obj
 {
     my $coo = shift;    # coo = class or object
@@ -39,25 +45,160 @@ sub _class_or_obj
     return $coo->instance();
 }
 
-# helper function to read paths to a hash-of-hash entry
-sub hash_path
+# return ref to hash-of-hash (hoh) entry one level down by key
+# private class function
+sub _descend_hoh
+{
+    my ( $node_ref, $key ) = @_;
+
+    if ( ref $node_ref eq "HASH" ) {
+        # descend into hash ref by string
+        if ( exists $node_ref->{ $key } ) {
+            if ( not ref $node_ref->{ $key } ) {
+                return \$node_ref->{ $key };
+            }
+            return $node_ref->{ $key };
+        } else {
+            return NotFound->err();
+        }
+    } elsif ( ref $node_ref eq "ARRAY" ) {
+        # descend into array ref by int
+        if ( _str_is_int( $key )) {
+            if ( exists $node_ref->[ $key ] ) {
+                if ( not ref $node_ref->[ $key ] ) {
+                    return \$node_ref->[ $key ];
+                }
+                return $node_ref->[ $key ];
+            } else {
+                return NotFound->err();
+            }
+        } else {
+            return NonIntegerIndex->err();
+        }
+    }
+
+    # any other ref type (or non-reference) is invalid for a tree node
+    return InvalidNodeType->err();
+}
+
+# find ref to hash-of-hash (hoh) node by a path
+# returns results ok() or err()
+# private class function
+sub _get_hoh_path
 {
     my ($class, @path) = @_;
     my $instance = __PACKAGE__->instance();
-    my $return_value = $instance->{ $path[0] };
-    for (1 .. (scalar @path - 1)) {
-        $return_value = $return_value->{ $path[$_] };
+    my $node_ref = $instance->{ $path[0] };
+
+    # descend tree to arbitrary depth as long as data exists for each key
+    for my $index ( 1 .. ( scalar @path - 1 )) {
+        my $node_result = _descend_hoh( $node_ref, $path[$index] );
+        if ( $node_result->is_ok()) {
+            # unwrap ref from result and continue
+            $node_ref = $node_result->unwrap();
+        } else {
+            # return error result
+            return $node_result;
+        }
     }
-    return $return_value;
+    return ok( $node_ref );
+}
+
+# make sure a path exists for writing to a possibly-new node
+# private class function
+sub _mk_hoh_path
+{
+    my ($class, @path) = @_;
+    my $instance = __PACKAGE__->instance();
+    my $node_ref = $instance->{ $path[0] };
+
+    # descend tree creating nodes if necessary
+    for my $index ( 1 .. ( scalar @path - 1 )) {
+        my $node_result = _descend_hoh( $node_ref, $path[$index] );
+        if ( $node_result->is_ok()) {
+            # unwrap ref from result and continue
+            $node_ref = $node_result->unwrap();
+        } else {
+            # for NotFound errors, create missing nodes if possible
+            my $node_err = $node_result->unwrap_err();
+            if ( $node_err->isa( NotFound )) {
+                if ( _str_is_int( $path[$index] )) {
+                    return NoAutoArray->err();
+                }
+                if ( ref $node_ref ne "HASH" ) {
+                    return InvalidNodeType->err();
+                }
+                $node_ref->{ $path[$index] } = {};
+                $node_ref = $node_ref->{ $path[$index] };
+            } else {
+                # return error result
+                return $node_result;
+            }
+        }
+    }
+    return ok( $node_ref );
 }
 
 # check for existence of a config entry
+# returns boolean: true if successful, false if item does not exist
+# public class method
 sub contains
 {
     my ( $class_or_obj, @keys ) = @_;
     my $instance = _class_or_obj($class_or_obj);
-    my $data = $instance->hash_path( @keys );
-    return defined $data;
+    my $hoh_result = $instance->_get_hoh_path( @keys );
+    return $hoh_result->is_ok() ? true : false;
+}
+
+# configuration read accessor
+# returns result type: ok(value) if successful, err(type) if item does not exist
+# public class method
+sub read_accessor
+{
+    my ( $class_or_obj, @keys ) = @_;
+    my $instance = _class_or_obj($class_or_obj);
+    my $hoh_result = $instance->_get_hoh_path( @keys );
+    if ( $hoh_result->is_err()) {
+        return $hoh_result;
+    }
+    my $value = $hoh_result->unwrap();
+    if ( ref $value eq "HASH" or ref $value eq "ARRAY" or not ref $value ) {
+        return ok($value);
+    }
+    return ok($$value);
+}
+
+# configuration write accessor
+# returns result type: ok() if successful, err(type) if item does not exist
+# public class method
+sub write_accessor
+{
+    my ( $class_or_obj, $keys_ref, $value ) = @_;
+    my $instance = _class_or_obj($class_or_obj);
+    my @keys = ( ref $keys_ref eq "ARRAY" ) ? @$keys_ref : $keys_ref;
+    my $last_key = pop @keys;
+    my $hoh_result = $instance->_mk_hoh_path( @keys );
+    if ( $hoh_result->is_err()) {
+        return $hoh_result;
+    }
+    my $node = $hoh_result->unwrap();
+    $node->{ $last_key } = $value;
+    return ok();
 }
 
 # TODO
+
+#
+# utility functions
+#
+
+# test if a string is a valid integer (all numeric)
+# private class function
+sub _str_is_int
+{
+    my $str = shift;
+    return true if $str =~ /^ \d+ $/x;
+    return false;
+}
+
+1;
