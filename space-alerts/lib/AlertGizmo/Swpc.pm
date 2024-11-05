407a70301f9160ab82e49dc82f1dde3b39320814
Ian Kluft <ko6yq.dev+github@gmail.com>
2024-11-05 15:41:43 -0800

    WIP: convert code from script: constants, pre_template()

--- space-alerts/lib/AlertGizmo/Swpc.pm
+++ space-alerts/lib/AlertGizmo/Swpc.pm
@@ -20,14 +20,85 @@ use builtin      qw(true false);
 use charnames qw(:loose);
 use Readonly;
 use Carp qw(croak confess);
+use FindBin;
+use Set::Tiny;
+use File::Slurp;
+use JSON;
 
+# constants
+Readonly::Scalar my $SWPC_JSON_URL => "https://services.swpc.noaa.gov/products/alerts.json";
+Readonly::Scalar my $OUTDIR        => $FindBin::Bin;
+Readonly::Scalar my $OUTJSON       => "swpc-data.json";
+Readonly::Scalar my $TEMPLATE      => "noaa-swpc-alerts.tt";
+Readonly::Scalar my $OUTHTML       => "noaa-swpc-alerts.html";
+Readonly::Scalar my $S_NONE        => "none";
+Readonly::Scalar my $S_ACTIVE      => "active";
+Readonly::Scalar my $S_INACTIVE    => "inactive";
+Readonly::Scalar my $S_CANCEL      => "cancel";
+Readonly::Scalar my $S_SUPERSEDE   => "supersede";
+Readonly::Hash my %MONTH_NUM => (
+    "jan" => 1,
+    "feb" => 2,
+    "mar" => 3,
+    "apr" => 4,
+    "may" => 5,
+    "jun" => 6,
+    "jul" => 7,
+    "aug" => 8,
+    "sep" => 9,
+    "oct" => 10,
+    "nov" => 11,
+    "dec" => 12,
+);
+Readonly::Scalar my $ISSUE_HEADER      => "Issue Time";
+Readonly::Scalar my $ORIG_ISSUE_HEADER => "Original Issue Time";
+Readonly::Scalar my $SERIAL_HEADER     => "Serial Number";
+Readonly::Array my @BEGIN_HEADERS => ( "Begin Time", "Valid From", );
+Readonly::Array my @END_HEADERS => ( "Valid To", "End Time", "Now Valid Until", );
+Readonly::Scalar my $EXTEND_SERIAL_HEADER => "Extension to Serial Number";
+Readonly::Scalar my $CANCEL_SERIAL_HEADER => "Cancel Serial Number";
+Readonly::Array my @INSTANTANEOUS_HEADERS =>
+    ( "Threshold Reached", "Observed", "IP Shock Passage Observed", );
+Readonly::Scalar my $HIGHEST_LEVEL_HEADER => "Highest Storm Level Predicted by Day";
+Readonly::Scalar my $RETAIN_TIME          => 12;    # hours to keep items after expiration
+Readonly::Array my @TITLE_KEYS => ( "SUMMARY", "ALERT", "WATCH", "WARNING", "EXTENDED WARNING" );
+Readonly::Array my @LEVEL_COLORS =>
+    ( "#bbb", "#F6EB14", "#FFC800", "#FF9600", "#FF0000", "#C80000" );    # NOAA scales
 
 # class method AlertGizmo (parent) calls before template processing
 sub pre_template
 {
     my $class = shift;
 
-    # TODO
+    # initialize globals
+    $class->params( [ "timestamp" ], dt2dttz($class->config_timestamp()) );
+    $class->params( [ "alerts" ], {} );
+    $class->params( [ "cancel" ], Set::Tiny->new() );
+    $class->params( [ "supersede" ], Set::Tiny->new() );
+
+    # clear destination symlink
+    my $outlink = $OUTDIR . "/" . $OUTJSON;
+    $class->paths( [ "outlink" ], $outlink );
+    if ( -e $outlink ) {
+        if ( not -l $outlink ) {
+            croak "destination file $outlink is not a symlink";
+        }
+    }
+    my $outjson = $outlink . "-" . $class->config_timestamp();
+    $class->paths( [ "outjson" ], $outjson );
+
+    # perform SWPC request
+    do_swpc_request();
+
+    # read JSON into template data
+    # in case of JSON error, allow these to crash the program here before proceeding to symlinks
+    my $json_path = $class->config_test_mode() ? $outlink : $outjson;
+    my $json_text = File::Slurp::read_file($json_path);
+    $class->params( [ "json" ], JSON::from_json $json_text );
+
+    # convert response JSON data to template-able result
+    process_alerts();
+
     return;
 }
 
